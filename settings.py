2e0c2187f8dd76e73e9b3000f75f2e5539ee1411
Kevin Dunn <kevin.dunn@connectmv.com>
2012-03-18 18:27:02 -0400

    Moved admin media location

--- settings.py
+++ settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = '/site_media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '-5mybfei)w7!r#@nhn=nr(7m4x1&e%tsu)6++$hhoz8orn=zkp'
